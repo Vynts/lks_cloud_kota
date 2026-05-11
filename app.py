c046bad950eaa0795367d23f2be08a3cac50a546
Erza <121083259+Vynts@users.noreply.github.com>
2026-05-11 11:16:51 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = os.getenv('secret_key')
 
 @app.route('/')
-def dasdhboard():
+def dashboard():
     search_q = request.args.get('q', '')
     data_files = get_all_files(search_q)
     
